2d190d10bdfb42b452cf76a0bc41eb84953799b6
razSunny <sunnyraj2222.sr671@gmail.com>
2024-09-29 22:50:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './pages/Header.jsx';
+import Main from './pages/Main.jsx';
+import Footer from './pages/Footer.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+      )
+}
+
+export default App
